98b7ab3f22d710431ec31d948132c22fe061f2a7
Hero2411 <antonino24buttice@gmail.com>
2023-05-09 16:04:27 +0200

    Update

--- hw1.js
+++ hw1.js
@@ -1,4 +1,28 @@
 const quote_url = "https://api.quotable.io/quotes/random?tags=philosophy&maxLength=100"
+const UPButton = document.querySelector('#upbtn');
+const MenuButton = document.querySelector('#menubtn');
+const MenuCloseButton = document.querySelector('#menux');
+const Nav = document.querySelector('nav');
+
+UPButton.addEventListener('click', topFunction);
+MenuButton.addEventListener('click', hammenu);
+MenuCloseButton.addEventListener('click', dishammenu);
+
+function hammenu() {
+    MenuButton.style.display = 'none';
+    MenuCloseButton.style.display = 'block';
+    Nav.style.display = "flex";
+    UPButton.style.zIndex = '0';
+    
+}
+
+function dishammenu() {
+    MenuButton.style.display = 'block';
+    MenuCloseButton.style.display = 'none';
+    Nav.style.display = "none";
+    UPButton.style.zIndex = '1';
+}
+
 
 
 
